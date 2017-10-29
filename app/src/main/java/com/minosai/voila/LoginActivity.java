ff5565ca6567969ea3c9cacd9ddea81e56040ddb
Yaswant Narayan <yoyaswant@gmail.com>
2017-10-29 10:15:27 +0530

    added new transaction activity

--- app/src/main/java/com/minosai/voila/LoginActivity.java
+++ app/src/main/java/com/minosai/voila/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.minosai.voila;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView signupLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +16,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().setTitle("Login");
+
+        signupLink = (TextView)findViewById(R.id.signuplink);
+
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/minosai/voila/MainActivity.java
+++ app/src/main/java/com/minosai/voila/MainActivity.java
@@ -1,13 +1,39 @@
 package com.minosai.voila;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("LoginStatus",MODE_PRIVATE);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        if(sharedPreferences.contains("isLoggedIn")){
+            if(!sharedPreferences.getBoolean("isLoggedIn",false)){
+                startActivity(intent);
+            }
+        }else {
+            startActivity(intent);
+        }
+
+        fab = (FloatingActionButton)findViewById(R.id.addTransactionFab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
